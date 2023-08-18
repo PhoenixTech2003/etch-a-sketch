b5f2dc56fbb6db2492a575ab89adadbb6abd171e
Chiyembekezo Chilembwe <phoenixtech2003@gmail.com.github.com>
2023-08-18 06:38:21 +0200

    Updated generateGridBoxes to render proper grid types

--- js/code.js
+++ js/code.js
@@ -1,20 +1,36 @@
-//query select the grid box class
-// get value for number of boxes in the grid eg 16 for 16 x 16
-//create a div element
-//add a class of grid to the new div element
-//loop to append to grid box
-    //while value of number of boxes > 0:
-    //append grid to grid box
-function generateGridBoxes(gridType = 16){
-    let numberOfBoxes = gridType * gridType;
-    let gridBoxDiv = document.querySelector('.grid-box');
-    while(numberOfBoxes > 0){
-        let box = document.createElement('div');
-        box.classList.add('grid')
-        gridBoxDiv.appendChild(box);
+//PSEUDO-CODE
+// generateGridBoxes Function takes in gridType:
+    // create variable to store number of squares for each row
+    // select class of gridbox in the html
+    // loop for the value of gridType to get the number of rows for the gridtype
+        // assign a class of row to each row created
+        // append each row to gridbox
+    //select all elements with a class of row
+    //forEach row:
+        //loop for the value of number of boxes
+            //create a new div
+            //assign a class of grid
+            //append each div to row
     
-        numberOfBoxes --;
+function generateGridBoxes(gridType = 20){
+    let numberOfBoxes = gridType;
+    let gridBoxDiv = document.querySelector('.grid-box');
+    while(gridType > 0){
+        let row =document.createElement('div');
+        row.classList.add('row');
+        gridBoxDiv.appendChild(row);
+        gridType --;
     }
+    let rows = document.querySelectorAll('.row');
+    rows.forEach((div) => {
+        let count = numberOfBoxes;
+        while (count > 0){
+            let box = document.createElement('div');
+            box.classList.add('grid')
+            div.appendChild(box);
+            count --;
+        }
+    });
 }
 
 function addHoverOnGridBox(){
